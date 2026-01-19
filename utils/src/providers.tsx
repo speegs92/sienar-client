6376766b0c1c3ad2bc125cfa25a01c790145f9f4
speegs92 <christian@levesque.dev>
2026-01-19 14:25:37 -0600

    added system allowing the registration of state providers so apps can be programmatically generated

---
+++ utils/src/providers.tsx
@@ -0,0 +1,31 @@
+﻿import type { ReactElement, ReactNode, ComponentType, PropsWithChildren } from 'react';
+
+const registry: ComponentType<PropsWithChildren>[] = [];
+
+/**
+ * Registers a provider that wraps around the root React application
+ *
+ * @param provider The provider to register
+ */
+export function registerProvider(provider: ComponentType<PropsWithChildren>) {
+	// Providers should be added backwards to ensure that <StrictMode> is always the root
+	registry.unshift(provider);
+}
+
+/**
+ * Builds the provider tree to wrap around the root React application
+ *
+ * @param rootContent The root React application to render inside the provider tree
+ */
+export function buildProviderTree(rootContent: ReactElement): ReactNode {
+	let providerTree: ReactNode|undefined = undefined;
+
+	registry.forEach(Component => {
+		const children = providerTree === undefined
+			? rootContent
+			: providerTree;
+		providerTree = <Component children={children}/>
+	})
+
+	return providerTree;
+}
