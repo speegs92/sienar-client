cf941830d6beaf1ca29abd7637937044ec809e99
speegs92 <christian@levesque.dev>
2026-01-19 14:04:36 -0600

    added playground app for development purposes

---
+++ playground/vite.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+			'@sienar/utils': resolve(__dirname, '../utils/src')
+		}
+	}
+});
