b95e8a97602301b4453bb929bfb65f2840b41eea
speegs92 <christian@levesque.dev>
2026-01-20 11:35:31 -0600

    added programmatic routing system

---
+++ utils/src/routing.ts
@@ -0,0 +1,93 @@
+﻿// noinspection SuspiciousTypeOfGuard
+
+import { createBrowserRouter } from 'react-router-dom';
+import { inject } from './di.ts';
+
+import type { ReactNode } from 'react'
+import type { RouteObject } from 'react-router-dom';
+import type { InjectionKey } from './di.ts';
+
+const routes = new Map<(InjectionKey<InjectionKey<ReactNode>>|InjectionKey<ReactNode>), Route[]>();
+
+export const ERROR_VIEW = Symbol() as InjectionKey<ReactNode>;
+
+/**
+ * Registers routes with the Sienar application
+ *
+ * @param layoutKey The key of the layout with which to render the routes
+ * @param items The routes to render
+ */
+export function registerRoutes(layoutKey: InjectionKey<InjectionKey<ReactNode>>|InjectionKey<ReactNode>, ...items: Route[]): void {
+	if (!routes.has(layoutKey)) {
+		routes.set(layoutKey, []);
+	}
+
+	routes.get(layoutKey)!.push(...items);
+}
+
+export function createRouter() {
+	const layoutRoutes: RouteObject[] = [];
+	const mappedRoutes = new Map<ReactNode, Route[]>();
+	const errorComponent = inject(ERROR_VIEW, true);
+
+	for (let [layoutKey, childRoutes] of routes) {
+		const injectedValue = inject(layoutKey);
+		const layout = typeof injectedValue === 'symbol'
+			? inject(injectedValue) as ReactNode
+			: injectedValue as ReactNode;
+
+		if (!mappedRoutes.has(layout)) {
+			mappedRoutes.set(layout, []);
+		}
+
+		mappedRoutes
+			.get(layout)!
+			.push(...childRoutes);
+	}
+
+	for (let [layout, childRoutes] of mappedRoutes) {
+		layoutRoutes.push({
+			path: '',
+			element: layout,
+			errorElement: errorComponent,
+			children: convertSienarRoutesToReactRoutes(childRoutes)
+		});
+	}
+
+	return createBrowserRouter(layoutRoutes);
+}
+
+export function convertSienarRoutesToReactRoutes(sienarRoutes: Route[]): RouteObject[] {
+	const reactRouterRoutes: RouteObject[] = [];
+
+	for (let route of sienarRoutes) {
+		const reactRouterRoute: RouteObject = {
+			path: typeof route.path === 'string' ? route.path : inject(route.path),
+			children: route.children ? convertSienarRoutesToReactRoutes(route.children) : undefined,
+			element: typeof route.element === 'symbol' ? inject(route.element) : route.element
+		}
+		reactRouterRoutes.push(reactRouterRoute);
+	}
+
+	return reactRouterRoutes;
+}
+
+/**
+ * A Sienar route object
+ */
+export interface Route {
+	/**
+	 * The route path. Can be either a string or an injection key pointing to a string
+	 */
+	path: string|InjectionKey<string>;
+
+	/**
+	 * The element to render. Can be either a {@link ReactNode} or an {@link InjectionKey} pointing to a {@see ReactNode}
+	 */
+	element: ReactNode|InjectionKey<ReactNode>;
+
+	/**
+	 * The child routes to render, if any
+	 */
+	children?: Route[];
+}
