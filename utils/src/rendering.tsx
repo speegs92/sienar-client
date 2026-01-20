8a4d7b8c7a4a69fd1b931d0f3be17cd41e9d4969
speegs92 <christian@levesque.dev>
2026-01-20 11:35:50 -0600

    added programmatic app rendering system

---
+++ utils/src/rendering.tsx
@@ -0,0 +1,21 @@
+﻿import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { createRouter } from './routing.ts';
+import { buildProviderTree } from './providers.tsx';
+
+import type { ReactElement } from 'react';
+
+export function useRerender(): [() => void, boolean] {
+	const [trigger, setTrigger] = useState(false);
+	return [() => setTrigger(!trigger), trigger];
+}
+
+export function createApp(rootId: string = 'root') {
+	createRoot(document.getElementById(rootId)!)
+		.render(buildProviderTree(createSienarRoot()));
+}
+
+function createSienarRoot(): ReactElement {
+	return <RouterProvider router={ createRouter() }/>;
+}
