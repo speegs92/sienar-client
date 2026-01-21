00182dc0f8e7c02d5f1019742730ae4868ef8b41
speegs92 <christian@levesque.dev>
2026-01-20 18:17:03 -0600

    added plugin infrastructure

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -1,5 +1,6 @@
 ﻿export * from '@utils/di.ts';
 export * from '@utils/menus.ts';
+export * from '@utils/plugins.ts';
 export * from '@utils/providers.tsx';
 export * from '@utils/rendering.tsx';
 export * from '@utils/routing.ts';
---
+++ utils/src/plugins.ts
@@ -0,0 +1,24 @@
+﻿const registry: Plugin[] = [];
+
+/**
+ * Registers plugins
+ *
+ * @param plugins The plugins to register
+ */
+export function registerPlugins(...plugins: Plugin[]) {
+	registry.push(...plugins);
+}
+
+/**
+ * Executes all registered plugins
+ */
+export function executePlugins() {
+	registry.forEach(p => p());
+}
+
+/**
+ * A client-side Sienar plugin
+ */
+export interface Plugin {
+	(): void;
+}
