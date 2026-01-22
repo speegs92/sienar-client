c8fb02a8fb5a19c46a7cd212075abf3f22179157
speegs92 <christian@levesque.dev>
2026-01-22 16:23:44 -0600

    added core plugin to playground app's registered plugins

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,7 +1,9 @@
 import { createApp, registerPlugins } from '@sienar/utils';
+import { plugin as corePlugin } from '@sienar/plugins-core';
 import { plugin as identityPlugin } from '@sienar/plugins-identity';
 
 registerPlugins(
+	corePlugin,
 	identityPlugin
 );
 
