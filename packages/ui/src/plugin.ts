69653715f56de41657ea0c287fdc0be0ed572c70
speegs92 <christian@levesque.dev>
2026-03-01 15:21:41 -0600

    moved old @sienar/ui code to old-src/ directory so it can be preserved while a new UI can be developed

--- packages/ui/src/plugin.ts
+++ packages/ui/src/plugin.ts
@@ -1,10 +1,5 @@
-﻿import { provide, NOTIFICATION_PROVIDER_COMPONENT, NOTIFIER } from '@sienar/utils';
-import SnackbarProvider from './SnackbarProvider.tsx';
-import { MUI_DATE_LOCALIZATION_PROVIDER, default as MuiDateLocalizationProvider } from './MuiDateLocalizationProvider.tsx';
-import { notifier } from './utils.ts';
+﻿
 
 export function plugin() {
-	provide(NOTIFICATION_PROVIDER_COMPONENT, SnackbarProvider);
-	provide(MUI_DATE_LOCALIZATION_PROVIDER, MuiDateLocalizationProvider);
-	provide(NOTIFIER, notifier);
+
 }
