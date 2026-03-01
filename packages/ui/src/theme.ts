fd1b86ac0f4fc61bc668327bb6808649c017382a
speegs92 <christian@levesque.dev>
2026-03-01 15:26:03 -0600

    added theme file

---
+++ packages/ui/src/theme.ts
@@ -0,0 +1,106 @@
+﻿import { createContext, useContext } from 'react';
+
+export const ThemeContext = createContext<Themeable>({});
+
+export const useThemeContext = () => useContext(ThemeContext);
+
+/**
+ * Creates a string of CSS class names based on the value of theme-based props
+ *
+ * @param color The selected theme color, if any
+ * @param variant The selected variant, if any
+ * @param base The base component CSS class name
+ *
+ * @returns The space-separated CSS class names
+ */
+export function createThemedClassNames(
+	color: Color|undefined,
+	variant: Variant|undefined,
+	base: string): string {
+	const classes = [ base ];
+
+	if (color && color !== 'default') classes.push(`${base}--${color}`);
+	if (variant) classes.push(`${base}--${variant}`);
+
+	return classes.join(' ');
+}
+
+
+/**
+ * The theme colors supported by Sienar UI
+ */
+export type Color =
+	| 'primary'
+	| 'secondary'
+	| 'tertiary'
+	| 'success'
+	| 'info'
+	| 'warning'
+	| 'error'
+	| 'light'
+	| 'dark'
+	| 'default';
+
+/**
+ * The color solidity variants supported by Sienar UI
+ */
+export type Variant =
+	| 'solid'
+	| 'outlined'
+	| 'text';
+
+/**
+ * The themeable props
+ */
+export type Themeable = {
+	/**
+	 * The themeable color
+	 */
+	color?: Color;
+
+	/**
+	 * The themeable variant
+	 */
+	variant?: Variant;
+}
+
+/**
+ * Horizontal alignment
+ */
+export type Alignment =
+	| 'left'
+	| 'right'
+	| 'center';
+
+/**
+ * Text alignment
+ */
+export type TextAlignment = Alignment | 'justify';
+
+/**
+ * Width breakpoints
+ */
+export type Breakpoint =
+	| 'sm'
+	| 'md'
+	| 'lg'
+	| 'xl'
+	| 'xxl';
+
+/**
+ * The available column sizes in the Sienar grid system
+ */
+export type ColumnSize =
+	| 1
+	| 2
+	| 3
+	| 4
+	| 5
+	| 6
+	| 7
+	| 8
+	| 9
+	| 10
+	| 11
+	| 12
+	| 'auto';
