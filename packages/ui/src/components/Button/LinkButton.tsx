268cdeec4daa631850dfc263dd8358ddecb98ef6
speegs92 <christian@levesque.dev>
2026-03-29 16:13:30 -0500

    updated <LinkButton> to use the correct attributes type

--- packages/ui/src/components/Button/LinkButton.tsx
+++ packages/ui/src/components/Button/LinkButton.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { inject } from '@sienar/utils';
 import { createButtonClasses } from './shared.ts';
 
-import type { ForwardedRef, HTMLAttributes } from 'react';
+import type { ForwardedRef, AnchorHTMLAttributes } from 'react';
 import type { InjectionKey } from '@sienar/utils';
 import type { ButtonBaseProps } from './shared.ts';
 
@@ -12,7 +12,7 @@ import type { ButtonBaseProps } from './shared.ts';
  */
 export interface LinkButtonProps extends
 	ButtonBaseProps,
-	Omit<HTMLAttributes<HTMLAnchorElement>, 'color'|'href'> {
+	Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'color'|'href'> {
 	/**
 	 * The link <code>href</code>
 	 */
