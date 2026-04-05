06b5f6fb6f6912370e4005d6af79ee2702838af5
speegs92 <christian@levesque.dev>
2026-04-05 15:41:41 -0500

    updated <Login> page to use new form components

--- packages/plugins-identity/src/views/account/Login.tsx
+++ packages/plugins-identity/src/views/account/Login.tsx
@@ -1,17 +1,15 @@
-﻿// import { useSearchParams } from 'react-router-dom';
-// import { ButtonLink, StandaloneCheckbox, Form, Textbox } from '@sienar/ui';
-// import { AuthorizeRoute, required, useNavigate, useAuthContext, useDocumentTitle } from '@sienar/utils';
-// import { MAIN_URL } from '@sienar/plugins-core';
-// import { ACCOUNT_LOCKED_URL, FORGOT_PASSWORD_URL } from '@plugins-identity/urls.ts';
-// import { LOGIN_SERVICE } from '@plugins-identity/services.ts';
-// import type { RequestResult } from '@sienar/utils';
-// import type { LoginResult } from '@plugins-identity/types.ts';
-import { LOGIN_URL } from '@plugins-identity/urls.ts';
+﻿import { useSearchParams } from 'react-router-dom';
+import { Icon, LinkButton, Form, StandaloneCheckbox, Textbox } from '@sienar/ui';
+import { AuthorizeRoute, required, useNavigate, useAuthContext, useDocumentTitle } from '@sienar/utils';
+import { MAIN_URL } from '@sienar/plugins-core';
+import { ACCOUNT_LOCKED_URL, FORGOT_PASSWORD_URL, LOGIN_URL } from '@plugins-identity/urls.ts';
 import { LOGIN_LAYOUT } from '@plugins-identity/layouts.ts';
+import { LOGIN_SERVICE } from '@plugins-identity/services.ts';
 
 import type { ReactNode } from 'react';
 import type { ViewModule } from '@sienar/plugins-core';
-import type { InjectionKey } from '@sienar/utils';
+import type { InjectionKey, RequestResult } from '@sienar/utils';
+import type { LoginResult } from '@plugins-identity/types.ts';
 
 /**
  * The content of the login page
@@ -19,68 +17,73 @@ import type { InjectionKey } from '@sienar/utils';
 export const LOGIN_VIEW = Symbol() as InjectionKey<ReactNode>;
 
 function Login() {
-	return <>Log in yo</>;
-	// useDocumentTitle('Log in');
-	// const navigate = useNavigate();
-	// const authContext = useAuthContext();
-	// const [ params ] = useSearchParams();
-	//
-	// const onLogin = async (result: RequestResult<LoginResult>) => {
-	// 	if (result.result) {
-	// 		const { userId, verificationCode } = result.result;
-	// 		navigate(ACCOUNT_LOCKED_URL, { userId, verificationCode });
-	// 	}
-	//
-	// 	if (!result.wasSuccessful) return;
-	//
-	// 	await authContext.loadUserData();
-	// 	const returnUrl = params.get('returnUrl');
-	//
-	// 	if (returnUrl) {
-	// 		params.delete('returnUrl');
-	// 		const queryParams = params.toString();
-	// 		navigate(`${returnUrl}?${queryParams}`)
-	// 	} else {
-	// 		navigate(MAIN_URL);
-	// 	}
-	// }
-	//
-	// return (
-	// 	<AuthorizeRoute mustBeLoggedOut>
-	// 		<Form
-	// 			serviceKey={LOGIN_SERVICE}
-	// 			title='Log in'
-	// 			submitText='Log in'
-	// 			onSuccess={onLogin}
-	// 			additionalActions={(
-	// 				<ButtonLink
-	// 					to={FORGOT_PASSWORD_URL}
-	// 					color='secondary'
-	// 					variant='outlined'
-	// 				>
-	// 					I forgot my password
-	// 				</ButtonLink>
-	// 			)}
-	// 		>
-	// 			<Textbox
-	// 				name='accountName'
-	// 				displayName='Username or email address'
-	// 				validators={[required()]}
-	// 				hideNonErrors
-	// 			/>
-	// 			<Textbox
-	// 				name='password'
-	// 				displayName='Password'
-	// 				type='password'
-	// 				validators={[required()]}
-	// 				hideNonErrors
-	// 			/>
-	// 			<StandaloneCheckbox name='rememberMe'>
-	// 				Remember me
-	// 			</StandaloneCheckbox>
-	// 		</Form>
-	// 	</AuthorizeRoute>
-	// );
+	useDocumentTitle('Log in');
+	const navigate = useNavigate();
+	const authContext = useAuthContext();
+	const [ params ] = useSearchParams();
+
+	const onLogin = async (result: RequestResult<LoginResult>) => {
+		if (result.result) {
+			const { userId, verificationCode } = result.result;
+			navigate(ACCOUNT_LOCKED_URL, { userId, verificationCode });
+		}
+
+		if (!result.wasSuccessful) return;
+
+		await authContext.loadUserData();
+		const returnUrl = params.get('returnUrl');
+
+		if (returnUrl) {
+			params.delete('returnUrl');
+			const queryParams = params.toString();
+			navigate(`${returnUrl}?${queryParams}`)
+		} else {
+			navigate(MAIN_URL);
+		}
+	}
+
+	return (
+		<AuthorizeRoute mustBeLoggedOut>
+			<Form
+				serviceKey={LOGIN_SERVICE}
+				title='Log in'
+				submitText='Log in'
+				onSuccess={onLogin}
+				additionalActions={(
+					<LinkButton
+						href={FORGOT_PASSWORD_URL}
+						color='secondary'
+						variant='outlined'
+					>
+						I forgot my password
+					</LinkButton>
+				)}
+			>
+				<Textbox
+					name='accountName'
+					displayName='Username or email address'
+					leftIcon={<Icon icon='account'/>}
+					validators={[required()]}
+					validationListProps={{
+						hideNonErrors: true
+					}}
+				/>
+				<Textbox
+					name='password'
+					displayName='Password'
+					type='password'
+					leftIcon={<Icon icon='lock'/>}
+					validators={[required()]}
+					validationListProps={{
+						hideNonErrors: true
+					}}
+				/>
+				<StandaloneCheckbox name='rememberMe' color='secondary'>
+					Remember me
+				</StandaloneCheckbox>
+			</Form>
+		</AuthorizeRoute>
+	);
 }
 
 const module: ViewModule = {
