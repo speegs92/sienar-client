720b8222035ead88608678f3e136a50dccedd2a5
speegs92 <christian@levesque.dev>
2026-06-17 18:46:50 -0500

    removed unnecessary dedupe

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
-		dedupe: [
-			'react',
-			'react-dom',
-			'react-router',
-			'react-router-dom'
-		],
 		alias: {
 			'@': resolve(__dirname, './src'),
 			'@utils': resolve(__dirname, './packages/utils/src'),
